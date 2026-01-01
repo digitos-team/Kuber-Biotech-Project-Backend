a6dbbed61dbbb4d8c7145c166aa2ff9fd61bbc31
Irfan Kazi <irfangithub123@gmail.com>
2026-01-01 10:37:43 +0530

    feat: add MongoDB connection utility and minor .gitignore formatting.

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 import { DB_Name } from "../constant.js"
- 
+
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect("mongodb://localhost:27017/"+ DB_Name)
-        console.log(`Database Connected To ${DB_Name} hosted by ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: process.env.appName
+        })
+        console.log(`Database Connected To ${process.env.appName} hosted by ${connectionInstance.connection.host}`);
     } catch (err) {
-        console.log(err);
+        console.log("MongoDB connection error: ", err);
         process.exit(1)
     }
 }
